936228bdfac29adf2bd261241da7c3107ccd7153
jermesonf <jemersonferreira747@gmail.com>
2024-06-01 23:39:25 -0300

    C#

---
+++ PrjArrayList1/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace PrjArrayList1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.txtValor1 = new System.Windows.Forms.TextBox();
+            this.btnProcurar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(25, 93);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(168, 20);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "Números Cadastrados";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2.Location = new System.Drawing.Point(25, 122);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(51, 20);
+            this.lbl2.TabIndex = 1;
+            this.lbl2.Text = "label2";
+            // 
+            // txtValor1
+            // 
+            this.txtValor1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtValor1.Location = new System.Drawing.Point(29, 163);
+            this.txtValor1.Name = "txtValor1";
+            this.txtValor1.Size = new System.Drawing.Size(164, 31);
+            this.txtValor1.TabIndex = 2;
+            // 
+            // btnProcurar
+            // 
+            this.btnProcurar.Location = new System.Drawing.Point(29, 211);
+            this.btnProcurar.Name = "btnProcurar";
+            this.btnProcurar.Size = new System.Drawing.Size(164, 45);
+            this.btnProcurar.TabIndex = 3;
+            this.btnProcurar.Text = "Procurar";
+            this.btnProcurar.UseVisualStyleBackColor = true;
+            this.btnProcurar.Click += new System.EventHandler(this.btnProcurar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(214, 279);
+            this.Controls.Add(this.btnProcurar);
+            this.Controls.Add(this.txtValor1);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Name = "Form1";
+            this.Text = "Listas";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.TextBox txtValor1;
+        private System.Windows.Forms.Button btnProcurar;
+    }
+}
+
---
+++ PrjArrayList1/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrjArrayList1
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        //Criado lista
+        List<int> lst = new List<int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lst.Add(5);
+            lst.Add(11);
+            lst.Add(20);
+
+
+            lbl2.Text = "";
+            for (int i = 0; i < lst.Count; i++)
+            {
+                lbl2.Text = lbl2.Text + lst.ElementAt(i) + ", ";
+            }
+        }
+
+        private void btnProcurar_Click(object sender, EventArgs e)
+        {
+            //Mostrar a posição que o elemento esta dentro da lista
+            int num = int.Parse(txtValor1.Text);
+            int pos = -1;
+
+            for (int i = 0; i < lst.Count; i++)
+            {
+                if(num == lst.ElementAt(i))
+                {
+                    pos = i;
+                }
+            }
+            MessageBox.Show(pos + "");
+
+        }
+    }
+}
